v-rob/minetest

commit eeb6cab4c4304cba92a77c2e759fcdde9dc0bcd1

In-game settings menu using separate Lua environment (#15614)

.luacheckrc
@@ -10,6 +10,7 @@ ignore = {
 read_globals = {
 	"ItemStack",
 	"INIT",
+	"PLATFORM",
 	"DIR_DELIM",
 	"dump", "dump2",
 	"fgettext", "fgettext_ne",
@@ -74,10 +75,6 @@ files["builtin/common/filterlist.lua"] = {
 files["builtin/mainmenu"] = {
 	globals = {
 		"gamedata",
-	},
-
-	read_globals = {
-		"PLATFORM",
 	},
 }
 

builtin/client/init.lua
@@ -2,7 +2,10 @@ local scriptpath = core.get_builtin_path()
 local clientpath = scriptpath.."client"..DIR_DELIM
 local commonpath = scriptpath.."common"..DIR_DELIM
 
-dofile(clientpath .. "register.lua")
+local builtin_shared = {}
+
+assert(loadfile(commonpath .. "register.lua"))(builtin_shared)
+assert(loadfile(clientpath .. "register.lua"))(builtin_shared)
 dofile(commonpath .. "after.lua")
 dofile(commonpath .. "mod_storage.lua")
 dofile(commonpath .. "chatcommands.lua")

builtin/common/register.lua
@@ -54,7 +54,8 @@ function builtin_shared.make_registration()
 	local registerfunc = function(func)
 		t[#t + 1] = func
 		core.callback_origins[func] = {
-			mod = core.get_current_modname() or "??",
+			-- may be nil or return nil
+			mod = core.get_current_modname and core.get_current_modname() or "??",
 			name = debug.getinfo(1, "n").name or "??"
 		}
 	end
@@ -66,7 +67,8 @@ function builtin_shared.make_registration_reverse()
 	local registerfunc = function(func)
 		table.insert(t, 1, func)
 		core.callback_origins[func] = {
-			mod = core.get_current_modname() or "??",
+			-- may be nil or return nil
+			mod = core.get_current_modname and core.get_current_modname() or "??",
 			name = debug.getinfo(1, "n").name or "??"
 		}
 	end

builtin/client/register.lua
@@ -1,68 +1,6 @@
-core.callback_origins = {}
+local builtin_shared = ...
 
-local getinfo = debug.getinfo
-debug.getinfo = nil
-
---- Runs given callbacks.
---
--- Note: this function is also called from C++
--- @tparam table  callbacks a table with registered callbacks, like `core.registered_on_*`
--- @tparam number mode      a RunCallbacksMode, as defined in src/script/common/c_internal.h
--- @param         ...       arguments for the callback
--- @return depends on mode
-function core.run_callbacks(callbacks, mode, ...)
-	assert(type(callbacks) == "table")
-	local cb_len = #callbacks
-	if cb_len == 0 then
-		if mode == 2 or mode == 3 then
-			return true
-		elseif mode == 4 or mode == 5 then
-			return false
-		end
-	end
-	local ret
-	for i = 1, cb_len do
-		local cb_ret = callbacks[i](...)
-
-		if mode == 0 and i == 1 or mode == 1 and i == cb_len then
-			ret = cb_ret
-		elseif mode == 2 then
-			if not cb_ret or i == 1 then
-				ret = cb_ret
-			end
-		elseif mode == 3 then
-			if cb_ret then
-				return cb_ret
-			end
-			ret = cb_ret
-		elseif mode == 4 then
-			if (cb_ret and not ret) or i == 1 then
-				ret = cb_ret
-			end
-		elseif mode == 5 and cb_ret then
-			return cb_ret
-		end
-	end
-	return ret
-end
-
---
--- Callback registration
---
-
-local function make_registration()
-	local t = {}
-	local registerfunc = function(func)
-		t[#t + 1] = func
-		core.callback_origins[func] = {
-			mod = core.get_current_modname() or "??",
-			name = getinfo(1, "n").name or "??"
-		}
-		--local origin = core.callback_origins[func]
-		--print(origin.name .. ": " .. origin.mod .. " registering cbk " .. tostring(func))
-	end
-	return t, registerfunc
-end
+local make_registration = builtin_shared.make_registration
 
 core.registered_globalsteps, core.register_globalstep = make_registration()
 core.registered_on_mods_loaded, core.register_on_mods_loaded = make_registration()

builtin/common/settings/init.lua
@@ -15,11 +15,11 @@
 --with this program; if not, write to the Free Software Foundation, Inc.,
 --51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-local path = core.get_mainmenu_path() .. DIR_DELIM .. "settings"
+local path = core.get_builtin_path() .. "common" .. DIR_DELIM .. "settings" .. DIR_DELIM
 
-dofile(path .. DIR_DELIM .. "settingtypes.lua")
-dofile(path .. DIR_DELIM .. "dlg_change_mapgen_flags.lua")
-dofile(path .. DIR_DELIM .. "dlg_settings.lua")
+dofile(path .. "settingtypes.lua")
+dofile(path .. "dlg_change_mapgen_flags.lua")
+dofile(path .. "dlg_settings.lua")
 
 -- Uncomment to generate 'minetest.conf.example' and 'settings_translation_file.cpp'.
 -- For RUN_IN_PLACE the generated files may appear in the 'bin' folder.

builtin/init.lua
@@ -78,6 +78,8 @@ elseif INIT == "client" then
 	dofile(scriptdir .. "client" .. DIR_DELIM .. "init.lua")
 elseif INIT == "emerge" then
 	dofile(scriptdir .. "emerge" .. DIR_DELIM .. "init.lua")
+elseif INIT == "pause_menu" then
+	dofile(scriptdir .. "pause_menu" .. DIR_DELIM .. "init.lua")
 else
 	error(("Unrecognized builtin initialization type %s!"):format(tostring(INIT)))
 end

builtin/mainmenu/init.lua
@@ -47,7 +47,7 @@ dofile(menupath .. DIR_DELIM .. "game_theme.lua")
 dofile(menupath .. DIR_DELIM .. "content" .. DIR_DELIM .. "init.lua")
 
 dofile(menupath .. DIR_DELIM .. "dlg_config_world.lua")
-dofile(menupath .. DIR_DELIM .. "settings" .. DIR_DELIM .. "init.lua")
+dofile(basepath .. "common" .. DIR_DELIM .. "settings" .. DIR_DELIM .. "init.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_create_world.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_delete_content.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_delete_world.lua")

builtin/pause_menu/init.lua
@@ -1,0 +1,12 @@
+local scriptpath = core.get_builtin_path()
+local pausepath = scriptpath.."pause_menu"..DIR_DELIM
+local commonpath = scriptpath.."common"..DIR_DELIM
+
+-- we're in-game, so no absolute paths are needed
+defaulttexturedir = ""
+
+local builtin_shared = {}
+
+assert(loadfile(commonpath .. "register.lua"))(builtin_shared)
+assert(loadfile(pausepath .. "register.lua"))(builtin_shared)
+dofile(commonpath .. "settings" .. DIR_DELIM .. "init.lua")

commit ae35167a5eae2f0f81ae63f2f0ed8049e018416f

Fix debug.getinfo not being unset in CPCSM (#16506)
regression was introduced in eeb6cab

.luacheckrc
@@ -41,7 +41,7 @@ stds.menu_common = {
 	},
 }
 
-files["builtin/client/register.lua"] = {
+files["builtin/client/init.lua"] = {
 	globals = {
 		debug = {fields={"getinfo"}},
 	}

builtin/client/init.lua
@@ -13,3 +13,6 @@ dofile(commonpath .. "information_formspecs.lua")
 dofile(clientpath .. "chatcommands.lua")
 dofile(clientpath .. "misc.lua")
 assert(loadfile(commonpath .. "item_s.lua"))({}) -- Just for push/read node functions
+
+-- unset, as promised in initializeSecurityClient()
+debug.getinfo = nil

builtin/common/register.lua
@@ -1,4 +1,5 @@
 local builtin_shared = ...
+local debug_getinfo = debug.getinfo
 
 do
 	local default = {mod = "??", name = "??"}
@@ -56,7 +57,7 @@ function builtin_shared.make_registration()
 		core.callback_origins[func] = {
 			-- may be nil or return nil
 			mod = core.get_current_modname and core.get_current_modname() or "??",
-			name = debug.getinfo(1, "n").name or "??"
+			name = debug_getinfo(1, "n").name or "??"
 		}
 	end
 	return t, registerfunc
@@ -69,7 +70,7 @@ function builtin_shared.make_registration_reverse()
 		core.callback_origins[func] = {
 			-- may be nil or return nil
 			mod = core.get_current_modname and core.get_current_modname() or "??",
-			name = debug.getinfo(1, "n").name or "??"
+			name = debug_getinfo(1, "n").name or "??"
 		}
 	end
 	return t, registerfunc

builtin/common/strict.lua
@@ -1,4 +1,4 @@
-local getinfo, rawget, rawset = debug.getinfo, rawget, rawset
+local debug_getinfo, rawget, rawset = debug.getinfo, rawget, rawset
 
 function core.global_exists(name)
 	if type(name) ~= "string" then
@@ -18,7 +18,7 @@ function meta:__newindex(name, value)
 	if declared[name] then
 		return
 	end
-	local info = getinfo(2, "Sl")
+	local info = debug_getinfo(2, "Sl")
 	if info ~= nil then
 		local desc = ("%s:%d"):format(info.short_src, info.currentline)
 		local warn_key = ("%s\0%d\0%s"):format(info.source, info.currentline, name)
@@ -36,7 +36,7 @@ function meta:__index(name)
 	if declared[name] then
 		return
 	end
-	local info = getinfo(2, "Sl")
+	local info = debug_getinfo(2, "Sl")
 	if info == nil then
 		return
 	end

builtin/game/register.lua
@@ -1,5 +1,6 @@
 local builtin_shared = ...
 local S = core.get_translator("__builtin")
+local debug_getinfo = debug.getinfo
 
 --
 -- Make raw registration functions inaccessible to anyone except this file
@@ -548,7 +549,7 @@ function core.registered_on_player_hpchange(player, hp_change, reason)
 		local func = core.registered_on_player_hpchanges.modifiers[i]
 		hp_change, last = func(player, hp_change, reason)
 		if type(hp_change) ~= "number" then
-			local debuginfo = debug.getinfo(func)
+			local debuginfo = debug_getinfo(func)
 			error("The register_on_hp_changes function has to return a number at " ..
 				debuginfo.short_src .. " line " .. debuginfo.linedefined)
 		end
@@ -570,7 +571,7 @@ function core.register_on_player_hpchange(func, modifier)
 	end
 	core.callback_origins[func] = {
 		mod = core.get_current_modname() or "??",
-		name = debug.getinfo(1, "n").name or "??"
+		name = debug_getinfo(1, "n").name or "??"
 	}
 end
 

builtin/profiler/instrumentation.lua
@@ -5,6 +5,7 @@
 local format, ipairs, type = string.format, ipairs, type
 local core, get_current_modname = core, core.get_current_modname
 local profiler, sampler = ...
+local debug_getinfo = debug.getinfo
 
 local instrument_builtin = core.settings:get_bool("instrument.builtin", false)
 
@@ -67,7 +68,7 @@ local worldmods_path = regex_escape(core.get_worldpath())
 local user_path = regex_escape(core.get_user_path())
 local builtin_path = regex_escape(core.get_builtin_path())
 local function generate_source_location(def)
-	local info = debug.getinfo(def.func)
+	local info = debug_getinfo(def.func)
 	local modpath = regex_escape(core.get_modpath(def.mod) or "")
 	local source = info.source
 	if modpath ~= "" then